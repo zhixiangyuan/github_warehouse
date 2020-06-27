6da6f0d5ce0c0438934233c675c0611de943a96e
xiangbei.yzx <xiangbei.yzx@alibaba-inc.com>
2020-06-27 18:04:17 +0800

    create repository

---
+++ book/JVM/recoverPdf.sh
@@ -0,0 +1,8 @@
+dir=$(pwd)
+filename=自己动手写Java虚拟机.pdf
+echo "${filename}_1 >> 自己动手写Java虚拟机.pdf"
+cat ${dir}/${filename}_1 >> ${dir}/${filename}
+echo "${filename}_2 >> 自己动手写Java虚拟机.pdf"
+cat ${dir}/${filename}_2 >> ${dir}/${filename}
+echo "${filename}_3 >> 自己动手写Java虚拟机.pdf"
+cat ${dir}/${filename}_3 >> ${dir}/${filename}
